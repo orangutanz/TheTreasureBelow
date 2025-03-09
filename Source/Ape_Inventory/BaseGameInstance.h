bddc23b574cfdf623616576bd07533f7ec316b84
Yuhan <admgmtt@gmail.com>
2025-03-09 20:54:19 +0800

    [MP level streaming]

--- Source/Ape_Inventory/BaseGameInstance.h
+++ Source/Ape_Inventory/BaseGameInstance.h
@@ -4,11 +4,21 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Engine/DataTable.h"
+#include "Engine/World.h"
 #include "BaseGameInstance.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct APE_INVENTORY_API FWorldReferenceStruct : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	// Soft reference to a level (UWorld)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UWorld> LevelReference;
+};
+
 UCLASS()
 class APE_INVENTORY_API UBaseGameInstance : public UGameInstance
 {
@@ -19,3 +29,4 @@ class APE_INVENTORY_API UBaseGameInstance : public UGameInstance
 	void Initialize();
 
 };
+
