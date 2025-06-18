de4abcd17c4c3e04efe6165dd283bdc3e5fd9917
Yuhan <admgmtt@gmail.com>
2025-06-18 23:26:26 +0800

    [Remove unused plugin]

--- Plugins/UtilityCombatPlugin/Source/UtilityCombatPlugin/Private/UtilityAIManagerToPawnInterface.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Zachary Kolansky, 2020
-
-
-#include "UtilityAIManagerToPawnInterface.h"
-
-// Add default functionality here for any IUtilityAIManagerToPawnInterface functions that are not pure virtual.
-
